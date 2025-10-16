d51213d3bf87a670582dbbf9b8ed462e68d301f7
hiker328 <marlonsouza328@gmail.com>
2025-10-16 17:45:49 -0300

    segundo commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
   },
 }
 
